0722a71bd112f6f51a95a3403f20197006402e67
akarahasan4 <akarahasan4@etf.unsa.ba>
2021-06-09 00:00:59 +0200

    Added productManagement.fxml

--- src/main/java/ba/unsa/etf/nrs/projekat/Controllers/ProductManagementController.java
+++ src/main/java/ba/unsa/etf/nrs/projekat/Controllers/ProductManagementController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +16,15 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class ProductManagementController {
 
     public Button btnBackProduct;
+    public ListView productList;
+    public TextField productNameFld;
+    public TextField quantityFld;
+    public TextField priceFld;
+    public TextField categoryFld;
+    public TextField discountFld;
+    public TextField barCodeFld;
+    public TextField discDiscountFld;
+    public TextField discCategoryFld;
 
     public void backAction(ActionEvent actionEvent) {
         Parent root = null;
@@ -28,5 +39,17 @@ public class ProductManagementController {
         primaryStage.show();
         Stage zatvori = (Stage) btnBackProduct.getScene().getWindow();
         zatvori.close();
+    }
+
+    public void changeBtn(ActionEvent actionEvent) {
+    }
+
+    public void addBtn(ActionEvent actionEvent) {
+    }
+
+    public void removeDiscount(ActionEvent actionEvent) {
+    }
+
+    public void setDiscount(ActionEvent actionEvent) {
     }
 }
